1eda8f2d57cbb423e4f4171bc863322dcf875557
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2025-09-27 00:46:04 +1000

    UI connected to config and .env

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID", "")
+REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET", "")
+REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT", "")
+MIN_SCORE = os.getenv("MIN_SCORE", "")
+MIN_LENGTH = os.getenv("MIN_LENGTH", "")
+SUBREDDITS = os.getenv("SUBREDDITS", "").split(",") if os.getenv("SUBREDDITS") else []
