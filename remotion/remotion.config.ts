e5f5086e9f3d4bd7704de08f831ed6297b6a8329
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2026-08-22 11:35:29 +1000

    Update remotion.config.ts

--- remotion/remotion.config.ts
+++ remotion/remotion.config.ts
@@ -5,12 +5,4 @@ Config.setOverwriteOutput(true);
 Config.setBrowserExecutable('/usr/bin/chromium');
 Config.setPublicDir('../');
 
-Config.setChromiumOptions((options) => {
-  return [
-    ...options,
-    '--ignore-gpu-blocklist',
-    '--enable-gpu-rasterization',
-    '--enable-zero-copy',
-    '--use-gl=egl'
-  ];
-});
+Config.setChromiumOpenGlRenderer('angle');
