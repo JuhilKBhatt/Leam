0d41a91eaedda5c9022b65306b13be980f4f5b45
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2026-08-23 12:11:21 +1000

    Update remotion.config.ts

--- remotion/remotion.config.ts
+++ remotion/remotion.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@remotion/cli/config';
 
 Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
-Config.setBrowserExecutable('/usr/bin/chromium');
+//Config.setBrowserExecutable('/usr/bin/chromium');
 Config.setPublicDir('../');
 
 Config.setChromiumOpenGlRenderer('angle');
