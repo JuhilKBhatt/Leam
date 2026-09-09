8b1bf559f6d4d84c0274c41a8fbb775bb866b6fe
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2026-09-09 12:42:15 +1000

    gpu support

--- modules/market_news/market_news_app.py
+++ modules/market_news/market_news_app.py
@@ -165,6 +165,7 @@ def run():
             f"--props={out_file}",
             "--concurrency=1",
             "--timeout=1200000",
+            "--gl=angle",
             "--scale=2",
             "--crf=14"
         ], cwd=remotion_dir, check=True)
